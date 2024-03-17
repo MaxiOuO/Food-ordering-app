7d5db60500d5c26c0805913eb149328689c9a485
Maxi Wang <wangyiwen1027@gmail.com>
2024-03-17 21:21:57 +0100

    Add menu item show list end point

--- src/app/menu-items/page.js
+++ src/app/menu-items/page.js
@@ -3,10 +3,21 @@ import { useProfile } from "@/components/UseProfile";
 import UserTabs from "@/components/layouts/UserTabs";
 import Link from "next/link";
 import Right from "@/components/icons/Right";
+import { useEffect, useState } from "react";
 
 
 export default function MenuItemsPage() {
+
     const { loading, data } = useProfile();
+    const [menuItems, setMenuItems] = useState('');
+
+    useEffect(() => {
+        fetch('/api/menu-items').then(res => {
+            res.json().then(menuItems => {
+                setMenuItems(menuItems);
+            })
+        })
+    }, [])
 
     if (loading) {
         return 'Loading user info...';
@@ -25,6 +36,11 @@ export default function MenuItemsPage() {
                     Create new menu item
                     <Right />
                 </Link>
+            </div>
+            <div>
+                {menuItems?.length > 0 && menuItems.map(item => (
+                    <div>{item.name}</div>
+                ))}
             </div>
         </section>
     )
