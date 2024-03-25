b870ff1458461a4545ad8fddc4988b43c68bf1c6
Maxi Wang <wangyiwen1027@gmail.com>
2024-03-25 19:12:47 +0100

    Fix menu page

--- src/components/menu/MenuItem.js
+++ src/components/menu/MenuItem.js
@@ -1,10 +1,19 @@
-export default function MenuItem({
-    name, image, description, basePrice, sizes, extraIngradientPrices
-}) {
+import { useContext } from "react"
+import { CartContext } from "../AppContext"
+
+export default function MenuItem(menuItem) {
+
+    const {
+        name, image, description, basePrice, sizes, extraIngradientPrices
+    } = menuItem;
+
+    const { addToCart } = useContext(CartContext);
+
     return (
         <div className="bg-gray-200 p-4 rounded-lg text-center hover:bg-white transition-all hover:shadow-md hover:shadow-black/25">
             <div className="text-center">
-                <img src={image} alt="pizza" className="max-h-auto max-h-24 block mx-auto" />
+                <img src={image} alt="pizza"
+                    className="max-h-auto max-h-24 block mx-auto" />
             </div>
             <h4 className="font-semibold my-3 text-xl">
                 {name}
@@ -12,7 +21,9 @@ export default function MenuItem({
             <p className="text-gray-500 text-sm line-clamp-3">
                 {description}
             </p>
-            <button className="bg-primary text-white rounded-full px-8 py-2 mt-4">
+            <button
+                onClick={() => addToCart(menuItem)}
+                className="bg-primary text-white rounded-full px-8 py-2 mt-4">
                 Add to cart €{basePrice}
             </button>
         </div>
